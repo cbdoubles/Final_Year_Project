00de42b50c57cc068949b1b73e8d7725fe1266dc
Nikola <104558252+nikolawow@users.noreply.github.com>
2024-05-10 16:03:14 +0200

    added buttons

--- frontend_next/pages/index.tsx
+++ frontend_next/pages/index.tsx
@@ -1,11 +1,13 @@
-import React, { useState, useEffect } from "react";
+// pages/index.tsx
+import React from 'react';
+import Button from '../src/utils/Button';
+import '../public/styles/style.css'; 
 
 export default function home() {
   return (
-    <>
-      <div>
-        <h1>Test</h1>
-      </div>
-    </>
+    <div className= "centered-column">
+      <Button title="Button 1" color="blue" size="70px" />
+      <Button title="Button 2" color="red" size="70px" />
+    </div>
   );
 }
