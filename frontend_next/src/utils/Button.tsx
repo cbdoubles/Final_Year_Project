cbf6153769fc9d095af614dba5ddd317dcdaf3d5
Nikola <104558252+nikolawow@users.noreply.github.com>
2024-05-10 16:03:14 +0200

    added buttons

--- frontend_next/src/utils/Button.tsx
+++ frontend_next/src/utils/Button.tsx
@@ -1,5 +1,18 @@
-const Button = ({title}:{title:string}) => {
-  return <button>{title}</button>;
+// src/utils/Button.tsx
+const Button = ({ title, color, size }: { title: string; color: string; size: string }) => {
+  return (
+    <button style={{
+      backgroundColor: color,
+      fontSize: size,
+      borderRadius: '50px', // This makes the button oval
+      padding: '10px 20px', // Adjust as needed
+      margin: '10px', // This adds space around the button
+      border: 'none', // Removes the default button border
+      color: 'white' // Changes the button text color to white
+    }}>
+      {title}
+    </button>
+  );
 }
 
 export default Button;
