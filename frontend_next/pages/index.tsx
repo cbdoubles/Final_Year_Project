df7665e8d6cb5bf73979fc8dd9b1a87fd8fc7451
Nikola <104558252+nikolawow@users.noreply.github.com>
2024-05-12 10:45:15 +0200

    added background image

--- frontend_next/pages/index.tsx
+++ frontend_next/pages/index.tsx
@@ -8,53 +8,69 @@ export default function home() {
   return (
     // <div className="centered-column">
     <div>
-      <style>{`
+      <style jsx global>{`
         body {
+          background-image: url(/images/background.jpg);
           background-size: cover;
+          background-repeat: no-repeat;
+          background-position: center;
+          background-attachment: fixed;
           color: white; 
           margin: 0; 
-          padding: 0; 
+          padding: 0;
+          height: 100vh; 
       `}</style>
 
       <div>
         <Image src={CapGemini} alt="Capgemini Logo" width={150} height={80} />
       </div>
       <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          justifyContent: "center",
-          height: "80vh",
-          backgroundColor: "#ADD8E6",
-          color: "white",
-        }}
+      style = {{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
       >
-        <button
+        <div
           style={{
-            marginBottom: "10px",
-            backgroundColor: "#0077C8",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "20vh",
+            width: "20vh",
+            backgroundColor: "#ADD8E6",
             color: "white",
-            width: "150px",
-            height: "30px",
-            border: "none",
-            borderRadius: "10px",
+            margin: "auto",
           }}
         >
-          Existing Project
-        </button>
-        <button
-          style={{
-            backgroundColor: "#0077C8",
-            color: "white",
-            width: "150px",
-            height: "30px",
-            border: "none",
-            borderRadius: "10px",
-          }}
-        >
-          New Project
-        </button>
+          <button
+            style={{
+              marginBottom: "10px",
+              backgroundColor: "#0077C8",
+              color: "white",
+              width: "150px",
+              height: "30px",
+              border: "none",
+              borderRadius: "10px",
+            }}
+          >
+            Existing Project
+          </button>
+          <button
+            style={{
+              backgroundColor: "#0077C8",
+              color: "white",
+              width: "150px",
+              height: "30px",
+              border: "none",
+              borderRadius: "10px",
+            }}
+          >
+            New Project
+          </button>
+        </div>
       </div>
     </div>
   );
