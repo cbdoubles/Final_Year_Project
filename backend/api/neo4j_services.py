a41172e864de42ef5fc18fe310721ee2db78d7f7
Kajetan Knopp <kajetan@knopp.com.pl>
2024-05-14 11:37:23 +0200

    Added query, which always works

--- backend/api/neo4j_services.py
+++ backend/api/neo4j_services.py
@@ -1,9 +1,8 @@
 from neo4j import GraphDatabase
 
 uri = "bolt://localhost:7687"
-user = "admin"
-password = "password"
-
+user = "neo4j"
+password = "password3"
 class Neo4jService:
     def __init__(self, uri, user, password):
         self.driver = GraphDatabase.driver(uri, auth=(user, password))
@@ -15,9 +14,22 @@ class Neo4jService:
         with self.driver.session() as session:
             result = session.run(query)
             return [record for record in result]
+        
+    def modify_query(query):
+        lower_query = query.lower()
+        
+        return_pos = lower_query.find("return")
+        
+        if return_pos != -1:
+            after_return = query[return_pos + 6:]
+            if "r" in after_return:
+                return query
+        modified_query = query[:-2] + "r" + query[-2:]
+        return modified_query
     
     def query_graph(self, nodes, edges, query):
         # Convert the nodes and edges to a format that can be used in a Cypher query
+        query = self.modify_query(query)
         nodes_param = [{**node} for node in nodes]
         edges_param = [{**edge} for edge in edges]
 
