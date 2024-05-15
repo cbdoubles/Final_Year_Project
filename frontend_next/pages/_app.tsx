f73ab1acce40566ea4d90d9f9659af762ff199df
Nikola <104558252+nikolawow@users.noreply.github.com>
2024-05-15 09:09:27 +0200

    updated the structure

---
+++ frontend_next/pages/_app.tsx
@@ -0,0 +1,23 @@
+import { AppProps } from "next/app";
+import Head from "next/head";
+import React, { useEffect } from "react";
+
+import "../public/styles/globals.css";
+import "../public/styles/style.css";
+
+const MyApp = ({ Component, pageProps }: AppProps<{}>) => {
+  return (
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=5, viewport-fit=cover"
+          name="viewport"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+};
+
+export default MyApp;
