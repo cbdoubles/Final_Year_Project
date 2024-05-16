4c7c6dccf39bcfc66a1a64140632350ae4d9c43f
Martijn Vermeulen <43009057+Martijn0405@users.noreply.github.com>
2024-05-16 09:26:44 +0200

    Select query (#6)
    * Initial implementation of react querycomponent
    
    * make select query folder
    
    Co-Authored-By: gvalkova <104890913+gvalkova@users.noreply.github.com>
    
    * header styling added
    
    "select favorite query is added " in grey
    
    Co-Authored-By: gvalkova <104890913+gvalkova@users.noreply.github.com>
    
    * button moved to the right
    
    Co-Authored-By: gvalkova <104890913+gvalkova@users.noreply.github.com>
    
    * Added trash bin button
    
    * Update styles.css
    
    * make items in list alternating styles
    
    Co-Authored-By: gvalkova <104890913+gvalkova@users.noreply.github.com>
    
    * Fixed error with tailwind not updating the output css
    
    * Improved query listing by quite a bit
    
    * Initial implementation of react querycomponent
    
    * make select query folder
    
    Co-Authored-By: gvalkova <104890913+gvalkova@users.noreply.github.com>
    
    * header styling added
    
    "select favorite query is added " in grey
    
    Co-Authored-By: gvalkova <104890913+gvalkova@users.noreply.github.com>
    
    * button moved to the right
    
    Co-Authored-By: gvalkova <104890913+gvalkova@users.noreply.github.com>
    
    * Added trash bin button
    
    * Update styles.css
    
    * make items in list alternating styles
    
    Co-Authored-By: gvalkova <104890913+gvalkova@users.noreply.github.com>
    
    * Fixed error with tailwind not updating the output css
    
    * Improved query listing by quite a bit
    
    ---------
    
    Co-authored-by: gvalkova <g.valkova@student.tue.nl>
    Co-authored-by: Mir Yousif <49961223+MirYousif@users.noreply.github.com>
    Co-authored-by: gvalkova <104890913+gvalkova@users.noreply.github.com>

---
+++ frontend_next/app/test/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+import SelectQuery from "@/src/components/select-query-window/SelectQuery";
+
+
+function TestPage() {
+    return <SelectQuery></SelectQuery>
+}
+
+export default TestPage;
