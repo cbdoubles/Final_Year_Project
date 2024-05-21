43a484ffbb07ab47f1643bd458406bc963821a9a
Mir Yousif <49961223+MirYousif@users.noreply.github.com>
2024-05-21 10:34:44 +0200

    files copied from DEMO
    neo4j services and upload file copied

--- backend/api/neo4j_services.py
+++ backend/api/neo4j_services.py
@@ -1,8 +1,9 @@
 from neo4j import GraphDatabase
+import re
 
 uri = "bolt://localhost:7687"
-user = "neo4j"
-password = "password2"
+user = "admin"
+password = "password"
 
 class Neo4jService:
     def __init__(self, uri, user, password):
@@ -16,24 +17,125 @@ class Neo4jService:
             result = session.run(query)
             return [record for record in result]
     
-    def query_graph(self, nodes, edges, query):
-        # Convert the nodes and edges to a format that can be used in a Cypher query
-        nodes_param = [{**node} for node in nodes]
-        edges_param = [{**edge} for edge in edges]
-
-        new_nodes = []
-        new_edges = []
-
-        with self.driver.session() as session:
-        # Run the provided query with the nodes and edges as parameters
-            result = session.run(query, {"nodes": nodes_param, "edges": edges_param})
-
-        # Process the result
+    def modify_query(self, query):
+        lower_query = query.lower()
         
-            for record in result:
-                if 'nodes' in record.keys():
-                   new_nodes.extend(record['nodes'])
-                if 'edges' in record.keys():
-                   new_edges.extend(record['edges'])
+        return_pos = lower_query.find("return")
+        
+        if return_pos != -1:
+            after_return = query[return_pos + 6:]
+            if "r" in after_return:
+                return query
+        modified_query = query + ",r" 
+        return modified_query
+    
+    def node_to_dict(self, node):
+        return {
+            'element_id': node.element_id,
+            'labels': list(node.labels),
+            'properties': dict(node.items())
+        }
+    
+    def rel_to_dict(self, rel):
+        return {
+        'element_id': rel.element_id,
+        'type': rel.type,
+        'properties': dict(rel.items()),
+        'start_node': self.node_to_dict(rel.start_node),
+        'end_node': self.node_to_dict(rel.end_node)
+        }
+    
+    # def query_graph(self, query):
+    #     query = self.modify_query(query)
 
-        return {"nodes": new_nodes, "edges": new_edges}
+    #     match = re.search('RETURN (.*)', query, re.IGNORECASE)
+    #     if match:
+    #         variables = match.group(1).split(',')
+    #         variables = [var.strip() for var in variables]
+
+    #     print(list(variables))
+
+    #     nodes = {}
+    #     final_nodes = {}
+    #     edges = {}
+
+    #     final_edges = {}
+
+    #     with self.driver.session() as session:
+    #         result = session.run(query)
+    #         for record in result:
+    #             for key in record.keys():
+    #                 name = record[key].element_id
+    #                 if(key[0] == "n" and name not in nodes):
+    #                     nodes[name] = record[key]
+
+    #                 if(key[0] == "r" and name not in edges):
+    #                     edges[name] = record[key]
+        
+    #     for node in nodes:
+    #         final_nodes[node] = self.node_to_dict(nodes[node])
+
+    #     for edge in edges:
+    #         start_id = edges[edge].start_node.element_id
+    #         end_id = edges[edge].end_node.element_id
+            
+    #         if(start_id not in nodes or end_id not in nodes):
+    #             continue
+
+    #         final_edges[edge] = self.rel_to_dict(edges[edge])
+            
+    #     return [list(final_nodes.values()), list(final_edges.values())]
+    
+        def extract_relations(text):
+            pattern = r'\((.*?)\)'
+            matches = re.findall(pattern, text)
+            return matches
+        
+        def extract_nodes(text):
+            pattern = r'\([.*?]\)'
+            matches = re.findall(pattern, text)
+            return matches
+    
+        def query_graph(self, query):
+            query = self.modify_query(query)
+
+            match = re.search('RETURN (.*)', query, re.IGNORECASE)
+            if match:
+                variables = match.group(1).split(',')
+                variables = [var.strip() for var in variables]
+
+            print(list(variables))
+
+            nodes = {}
+            final_nodes = {}
+            edges = {}
+
+            final_edges = {}
+
+            relation = self.extract_relations(query)[0]
+            node = self.extract_nodes(relation)[0]
+
+            with self.driver.session() as session:
+                result = session.run(query)
+                for record in result:
+                    for key in record.keys():
+                        name = record[key].element_id
+                        if(key[:len(node)] == node and name not in nodes):
+                            nodes[name] = record[key]
+
+                        if(key[:len(relation)] == relation and name not in edges):
+                            edges[name] = record[key]
+            
+            for node in nodes:
+                final_nodes[node] = self.node_to_dict(nodes[node])
+
+            for edge in edges:
+                start_id = edges[edge].start_node.element_id
+                end_id = edges[edge].end_node.element_id
+                
+                if(start_id not in nodes or end_id not in nodes):
+                    continue
+
+                final_edges[edge] = self.rel_to_dict(edges[edge])
+                
+            return [list(final_nodes.values()), list(final_edges.values())]
