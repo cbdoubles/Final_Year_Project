c5b203e3de8ed38731dbdc759aa5ac28034ffc69
cbdoubles <95439834+cbdoubles@users.noreply.github.com>
2024-05-22 10:07:55 +0200

    update import path for selectquery in projectpage view

--- frontend_next/src/views/ProjectFavourite.tsx
+++ frontend_next/src/views/ProjectFavourite.tsx
@@ -1,7 +1,7 @@
 // FavouritePage.js
 import React from "react"
 import ProjectPageView from "@/views/ProjectPageView"
-import SelectQuery from "@/src/views/Select"
+import SelectQuery from "@/components/select/Select"
 
 const FavouritePage = () => {
   return (
