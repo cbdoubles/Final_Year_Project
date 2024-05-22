44a98fc69e454620a8f372366570666912d94df2
Deni <d.gincheva@student.tue.nl>
2024-05-22 14:32:05 +0200

    added temporary Niko's model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
+# OLD MODEL 
+class Query(models.Model):
+    cypher_query = models.TextField()
+    natural_query = models.TextField()
+    graph = models.TextField()  # if you want to store the graph as a text file, we can do binary as well
+
+    def __str__(self):
+        return self.cypher_query
+
+# NEW MODEL
 class Project(models.Model):
     """
     Represents a project. Each project has a uiniqe name.
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'querify_db',
+        'USER': 'querify_remote',
+        'PASSWORD': 'querifyrocks',
+        'HOST': '145.220.75.94',  
+        'PORT': '3306',
     }
 }
 
