f5e256f6289d8f88e6c62178d9f16479b8336208
AgnesVulpescu <a.vulpescu@student.tue.nl>
2024-05-22 10:57:40 +0200

    Revert "switch mode button"
    This reverts commit 34117e89728d30b88962c8935a43a0e0833525cf.

--- frontend_next/src/utils/sideBar/SwitchModeIcon.tsx
+++ frontend_next/src/utils/sideBar/SwitchModeIcon.tsx
@@ -1,28 +1,34 @@
-import React, { useState } from "react";
-import { UserCircleIcon, UserPlusIcon, UserMinusIcon } from "@heroicons/react/24/outline";
+import React, { useState } from "react"
+/*import { UserCircleIcon } from "@heroicons/react/24/outline"*/
+import { UserCircleIcon, UserPlusIcon } from "@heroicons/react/24/outline"
+import { UserMinusIcon } from "@heroicons/react/24/outline"
+/*import DefaultIcon from  '/public/sidebarImages/defaulticon.png';*/
+
+//Detail for icon
+const properties = {
+  name: "Switch Mode",
+
+  href: "/sideBar",
+  icon: UserCircleIcon
+}
 
 export default function SwitchMode() {
   // State to manage the current icon
-  const [isUserPlus, setIsUserPlus] = useState(true);
-  
-  // State to manage the current mode name
-  const [modeName, setModeName] = useState("Switch to Basic Mode");
+  const [isUserPlus, setIsUserPlus] = useState(true)
 
-  // Function to handle the icon switch and mode name change
+  // Function to handle the icon switch
   const handleSwitchMode = () => {
-    setIsUserPlus(prevState => !prevState);
-    setModeName(prevName => (prevName === "Switch to Basic Mode" ? "Switch to Advanced Mode" : "Switch to Basic Mode"));
-  };
-
-  const LinkIcon = isUserPlus ? UserPlusIcon : UserMinusIcon;
-
+    setIsUserPlus((prevState) => !prevState)
+  }
+  const LinkIcon = isUserPlus ? UserPlusIcon : UserMinusIcon
   return (
     <a
+      key={properties.name}
       onClick={handleSwitchMode}
       className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium text-black hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
     >
       <LinkIcon className="w-6" />
-      <p className="hidden md:block">{modeName}</p>
+      <p className="hidden md:block">{properties.name}</p>
     </a>
-  );
+  )
 }
