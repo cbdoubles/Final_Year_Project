6fc79b78ed5f4da10266ebf326ac38e209fcf2ff
dauakarajeanes <104891093+dauakarajeanes@users.noreply.github.com>
2024-05-23 11:26:36 +0200

    Update spec.cy.ts
    - Added comments

--- frontend_next/cypress/e2e/spec.cy.ts
+++ frontend_next/cypress/e2e/spec.cy.ts
@@ -4,14 +4,10 @@ describe("template spec", () => {
     cy.visit("/")
   })
 
-  it("passes", () => {
-    cy.get('[data-frame="header-1"]').should("contain.text", "Querify")
-  })
-
+  //Test if buttons are visable
   it("should display the buttons", () => {
     // Check if the buttons are displayed and have the correct text
     cy.contains("button", "Existing Project").should("be.visible")
-
     cy.contains("button", "New Project").should("be.visible")
   })
 })
