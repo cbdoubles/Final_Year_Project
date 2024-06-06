68cfc347a9dc7d1f5c7109dec0805c16f24f7c0a
Nikola <104558252+nikolawow@users.noreply.github.com>
2024-06-06 17:34:05 +0200

    example-bhuvan-test

--- frontend_react_cypher_linter/linter/src/App.js
+++ frontend_react_cypher_linter/linter/src/App.js
@@ -1,9 +1,34 @@
-import './App.css';
-import Linter from './Linter';
+import "./App.css";
+import Linter from "./Linter";
+import { NeoGraph, ResponsiveNeoGraph } from "./neo4jVis";
+
+const NEO4J_URI = "bolt://localhost:7687";
+const NEO4J_USER = "neo4j";
+const NEO4J_PASSWORD = "cobra-paprika-nylon-conan-tobacco-2599";
 
 function App() {
   return (
-    <Linter />
+    <div>
+      <Linter />
+      <h1>React Neovis Example</h1>
+      {/* <ResponsiveNeoGraph
+        containerId={"id0"}
+        neo4jUri={NEO4J_URI}
+        neo4jUser={NEO4J_USER}
+        neo4jPassword={NEO4J_PASSWORD}
+        width={400}
+        height={300}
+      /> */}
+      <NeoGraph
+        width={400}
+        height={300}
+        containerId={"id1"}
+        neo4jUri={NEO4J_URI}
+        neo4jUser={NEO4J_USER}
+        neo4jPassword={NEO4J_PASSWORD}
+        backgroundColor={"#b2beb5"}
+      />
+    </div>
   );
 }
 
