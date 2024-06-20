41627de9ade5865c1c44d46f9aeb5bfd7191aaa5
cbdoubles <95439834+cbdoubles@users.noreply.github.com>
2024-06-20 20:27:41 +0200

    small bug - add projectId to InfoCard
    want to switch branches so sdoing small update :)

--- frontend_next/src/components/projectInfoCard/ProjectInfoCard.tsx
+++ frontend_next/src/components/projectInfoCard/ProjectInfoCard.tsx
@@ -10,6 +10,7 @@ const ProjectInfoCard = () => {
     <Card className="justify-center p-4 rounded-lg w-auto h-20">
       <p className="text-sm text-black">Project: {`${projectName}`}</p>
       <p className="text-sm text-black">Graph File: {`${graphName}`}</p>
+      <p className="text-sm text-black">Project ID: {`${projectId}`}</p>
       <div className="flex justify-end w-full">
         <button onClick={() => alert("Edit button clicked")}>
           <LuPenSquare className="h-5 w-5 mr-2 cursor-pointer" />
