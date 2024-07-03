0ca393b5954108b06d94847fadd0d145dde3bd83
KappaHolmes <s.dembowski@student.tue.nl>
2024-07-03 16:14:51 +0200

    Added contracts and cleanup to DisplayFolde rQUeries last 4 files

--- frontend_next/src/utils/displayFolderQueries/folderQueryFunctionality/QueryDisplay.tsx
+++ frontend_next/src/utils/displayFolderQueries/folderQueryFunctionality/QueryDisplay.tsx
@@ -3,7 +3,28 @@ import { FolderType, QueryType } from "@/src/libs/types";
 import EditQuery from "./EditQuery";
 import DeleteQuery from "./DeleteQuery";
 import { Button } from "@nextui-org/button";
-import { useState } from "react";
+import { React, useState } from "react";
+
+/**
+ * @description
+ * QueryDisplay Component
+ * This component is responsible for displaying a list of queries. It allows for each query to be interacted with - specifically,
+ * queries can be clicked on, edited, or deleted. Additionally, if enabled, queries can be shared. The component visually
+ * differentiates between even and odd queries for better readability and provides a visual indication of the currently selected query.
+ *
+ * @props
+ * @param {QueryType[]} queries - The list of queries to be displayed.
+ * @param {boolean} canBeShared - Indicates if the queries can be shared.
+ * @param {(query: QueryType) => void} handlerClick - Function to handle clicks on queries, typically to view or execute them.
+ * @param {(deletingQuery: boolean, deleteQuery: QueryType) => void} deleteQuery - Function to delete a specific query.
+ * @param {(editingQuery: boolean, editQuery: QueryType) => void} editQuery - Function to edit a specific query.
+ * @param {FolderType} type - The type of folder the queries belong to, used for categorization or filtering.
+ * @param {number | null} selectedButtonId - The ID of the currently selected button, if any.
+ * @param {React.Dispatch<React.SetStateAction<number | null>>} setSelectedButtonId - Function to set the ID of the selected button.
+ *
+ * @state
+ * @param {number | null} pressedButtonId - State to track the ID of the pressed query button. Null if no button is pressed. This is used to visually indicate the currently active or selected query.
+ */
 
 const QueryDisplay = ({
   queries,
@@ -12,8 +33,6 @@ const QueryDisplay = ({
   deleteQuery,
   editQuery,
   type,
-  selectedButtonId,
-  setSelectedButtonId,
 }: {
   queries: QueryType[];
   canBeShared: boolean;
@@ -30,20 +49,20 @@ const QueryDisplay = ({
     <>
       {queries.map((query, key) => (
         <div
+          key={query.queryId}
           className={`w-full flex items-center px-8 justify-between ${
             key % 2 === 0 ? "bg-sky-100" : "bg-white"
           }`}
-          key={query.queryId}
         >
           <Button
             className={`flex w-full gap-1 justify-start text-lg ${
-              selectedButtonId === query.queryId
+              pressedButtonId === query.queryId
                 ? "bg-blue-500"
                 : "bg-transparent"
             }`}
             onClick={() => {
               handlerClick(query);
-              setSelectedButtonId((prevId) =>
+              setPressedButtonId((prevId) =>
                 prevId === query.queryId ? null : query.queryId
               );
             }}
@@ -59,8 +78,8 @@ const QueryDisplay = ({
                 <LuShare className="text-black" />
               </button>
             )}
-            <EditQuery query={query} editQuery={editQuery} type={type} />
-            <DeleteQuery query={query} deleteQuery={deleteQuery} type={type} />
+            <EditQuery editQuery={editQuery} query={query} type={type} />
+            <DeleteQuery deleteQuery={deleteQuery} query={query} type={type} />
           </div>
         </div>
       ))}
